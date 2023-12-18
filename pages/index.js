b7e8ba1fd7850356d168f0c5ede3312afe3176c0
tamerlan-musayev <timoxa_musaev@mail.ru>
2023-12-18 15:01:01 +0400

    test

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <main>
+        <a href="https://1point.az/Q1sPI">
+          I am short link click me
+        </a>
+
+        <a href="sima://web-to-app?data=https://api.onepoint.az/web-sima-sign/v1/?tsquery=eyJTaWduYWJsZUNvbnRhaW5lciI6eyJQcm90b0luZm8iOnsiTmFtZSI6IndlYjJhcHAiLCJWZXJzaW9uIjoiMS4zIn0sIk9wZXJhdGlvbkluZm8iOnsiVHlwZSI6IkF1dGgiLCJPcGVyYXRpb25JZCI6IjAxMTllMmNjLTAzNDAtNDQ3NC05OTdkLWY5NDRmZTdkYWQwNSIsIk5iZlVUQyI6MTcwMjczNDQzOSwiRXhwVVRDIjoxNzAyODIwODM5LCJBc3NpZ25lZSI6W119LCJEYXRhSW5mbyI6eyJEYXRhVVJJIjoiaHR0cHM6Ly9hcGkub25lcG9pbnQuYXovd2ViLXNpbWEtc2lnbi92MS9nZXRkYXRhLzAxMTllMmNjLTAzNDAtNDQ3NC05OTdkLWY5NDRmZTdkYWQwNS9hdXRoP3VzZXJfaWQ9Y2MzNjY3ZDMtNDg3Ni00MjNhLWIxMjctYmMyN2FkMTk2MTUzJnRva2VuPWZjYTMyZjE5YmZhZTMwYTBjOWRhNzNkNThkYjVlYTc5NTRmM2Q3ZDhlY2NlOTUwNjkyMzZkMGQwYjU4NTVhYTUifSwiQ2xpZW50SW5mbyI6eyJDbGllbnRJZCI6MjEzNTAwMiwiQ2xpZW50TmFtZSI6Ik9uZVBvaW50IiwiSWNvblVSSSI6Imh0dHBzOi8vb25lcG9pbnRidWNrZXQuczMuZXUtY2VudHJhbC0xLmFtYXpvbmF3cy5jb20vYnJpZ2h0Mi5qcGciLCJDYWxsYmFjayI6Imh0dHBzOi8vYXBpLm9uZXBvaW50LmF6L3dlYi1zaW1hLXNpZ24vdjEvY2FsbGJhY2svMDExOWUyY2MtMDM0MC00NDc0LTk5N2QtZjk0NGZlN2RhZDA1L2F1dGg/dXNlcl9pZD1jYzM2NjdkMy00ODc2LTQyM2EtYjEyNy1iYzI3YWQxOTYxNTMmdG9rZW49ZmNhMzJmMTliZmFlMzBhMGM5ZGE3M2Q1OGRiNWVhNzk1NGYzZDdkOGVjY2U5NTA2OTIzNmQwZDBiNTg1NWFhNSIsIlJlZGlyZWN0VVJJIjoib25lcG9pbnQ6Ly93ZWItdG8tYXBwIn19LCJIZWFkZXIiOnsiQWxnTmFtZSI6IkhNQUNTSEEyNTYiLCJTaWduYXR1cmUiOiJlbnBPVVFJajVWcUZJdWZBT3Z1djFaUXVsR09objg5VjVLcGFJU21ZWCtnPSJ9fQ==">
+          I am short link click me
+        </a>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
